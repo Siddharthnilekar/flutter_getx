0a889a79ba90dbaa1ec898840b6ec90c8dbdec26
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2025-06-26 11:47:25 +0530

    Chapter 7: Integrating APIs with GetX

---
+++ lib/app/modules/product/modules/product/controllers/cart_controller.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+import 'package:getx_chapter_1/app/services/cart_service.dart';
+
+import '../../../models/product_model.dart';
+
+class CartController extends GetxController {
+  final CartService cartService = Get.find<CartService>();
+
+  List<ProductModel> get cartItems => cartService.cartItems;
+
+  double get totalAmount => cartService.totalAmount;
+
+  void addToCart(ProductModel product) {
+    cartService.addToCart(product);
+  }
+
+  void removeFromCart(ProductModel product) {
+    cartService.removeFromCart(product);
+  }
+}

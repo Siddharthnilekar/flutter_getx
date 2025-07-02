70adce4b7c226f70b71f5c5de51a77a14f1f2646
Siddharthnilekar <nilekarsiddharth26@gmail.com>
2025-07-02 11:42:01 +0530

    adjust checkout views font and theme

--- lib/app/services/theme_service.dart
+++ lib/app/services/theme_service.dart
@@ -1,23 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-
-// class ThemeService {
-//   final _key = 'isDarkMode';
-
-//   bool _loadThemeFromStorage() => Get.isDarkMode;
-
-//   void _saveThemeToStorage(bool isDarkMode) =>
-//       Get.changeThemeMode(isDarkMode ? ThemeMode.dark : ThemeMode.light);
-
-//   ThemeMode get theme =>
-//       _loadThemeFromStorage() ? ThemeMode.dark : ThemeMode.light;
-
-//   void switchTheme() {
-//     Get.changeThemeMode(Get.isDarkMode ? ThemeMode.light : ThemeMode.dark);
-//     _saveThemeToStorage(!Get.isDarkMode);
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
